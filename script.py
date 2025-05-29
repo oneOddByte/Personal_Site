9625c902c77b512df2d6ef743bd1f3803efb2786
Krish <git@github.com:dude121006/pwaTest.git>
2025-05-29 19:06:45 +0530

    added roadmaps

--- script.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-# subprocess.run(["git add ."])
-
-with open("count.txt", "w+") as file:
-    num = int(file.read())
-    # subprocess.run([f'git commit -m "{num + 1}"'])
-    file.write(str(num + 1))
